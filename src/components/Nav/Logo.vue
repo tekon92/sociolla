6806406ff290f47e974a76d8d28b912f8acfaa84
rian pauzi <rianpauzi@rians-MacBook-Pro.local>
2020-03-18 05:12:57 +0700

    change nav and jobs

--- src/components/Nav/Logo.vue
+++ src/components/Nav/Logo.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="flex flex-shrink md:w-1/3 justify-center md:justify-start text-white">
-    <router-link to="/">LOGO</router-link>
-  </div>
+<div class="flex items-center justify-between mb-4 md:mb-0">
+      <h1 class="leading-none text-2xl text-grey-darkest">
+        <router-link class="no-underline text-grey-darkest hover:text-black" to="/">LOGO</router-link>
+      </h1>
+
+      <a class="text-white hover:text-orange md:hidden" href="#">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/></svg>
+      </a>
+    </div>
 </template>
 
 <script>
